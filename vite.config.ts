47db37656756176c22f76a4504d6509d6ef162fb
TouristOnAMission <maikl.mikhael@yandex.ru>
2024-04-06 01:51:48 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Chess/',
   server: {
     port: 3000,
     host: '0.0.0.0',
